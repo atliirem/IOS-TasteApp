a97dfec536414296220ad35f47c11c4b8d8a3744
atliirem <nirematli@gmail.com>
2026-03-10 11:34:16 +0300

    Initial Commit

---
+++ TasteApp/TasteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteAppApp.swift
+//  TasteApp
+//
+//  Created by İrem Atlı on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TasteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
